097973bf211dde1b6db361f7e92f7b967ecba60d
최재호 <choijaeho@choejaehoui-MacBookPro.local>
2024-03-29 16:57:43 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManualComponent } from './manual/manual.component';
+import { CliComponent } from './cli/cli.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ManualComponent, CliComponent], // declarations에 components class를 추가한다.
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/manual/manual.component.ts
@@ -0,0 +1,8 @@
+// src/app/manual/manual.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual', // component명칭을 작성한다.
+  templateUrl: './manual.component.html', // 해당 component가 사용할 html 템플릿 주소를 입력한다.
+})
+export class ManualComponent {}
